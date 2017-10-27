1f322009c2316e9b1246f40a63e4b42f7efd8602
Matthew Hesketh <wrxck@users.noreply.github.com>
2017-10-27 19:20:04 +0100

    Create sort.lua

---
+++ lua/sort.lua
@@ -0,0 +1,19 @@
+local sort = function(tab)
+    if type(tab) ~= 'table' then
+        return false
+    end
+    local active = true
+    local temp = 0
+    while active do
+        active = false
+        for i = 1, #tab - 1 do
+            if tab[i + 1] < tab[i] then
+                temp = tab[i]
+                tab[i] = tab[i + 1]
+                tab[i + 1] = temp
+                active = true
+            end
+        end
+    end
+    return tab
+end
